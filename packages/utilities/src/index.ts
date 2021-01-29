5f1c596c588255ab0cb5bef66358a0928d4938b4
Jordan Jones <jordpjones@gmail.com>
2021-01-29 01:50:43 -0800

    utilities package

---
+++ packages/utilities/src/index.ts
@@ -0,0 +1,5 @@
+export * from './library/mergeDefault';
+export * from './library/deepClone';
+export * from './library/isObject';
+export * from './library/isPrimitive';
+export * from './library/utilityTypes';
